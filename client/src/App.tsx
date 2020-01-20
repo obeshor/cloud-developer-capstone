14e18d8ea06f74c0600301be8f66a606660460d2
Yannick Serge <yannicksergeobam@gmail.com>
2020-01-20 21:49:38 +0100

    update view

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -39,7 +39,7 @@ export default class App extends Component<AppProps, AppState> {
         <Segment style={{ padding: '8em 0em' }} vertical>
           <Grid container stackable verticalAlign="middle">
             <Grid.Row>
-              <Header as="h1">Monogram</Header>
+              <Header as="h1">eJournal App</Header>
             </Grid.Row>
             <Grid.Row>
               <Grid.Column width={16}>
